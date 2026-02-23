12c1005ba1c0da8fc62da088347f73f2b34036c3
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-02-23 19:06:36 +0100

    added first bunch of tests for Schnorr group gener ation

---
+++ tests/test_groupgen.py
@@ -0,0 +1,25 @@
+import pytest
+import src.crypto_utils
+from cryptography.hazmat.primitives.asymmetric import dsa
+
+
+# Test the generation of Schnorr group parameters
+def test_generate_schnorr_group():
+    p, q, g = src.crypto_utils.generate_schnorr_group()
+    assert p.bit_length() == 3072
+    assert q.bit_length() == 256
+    assert g > 1 and g < p
+
+# Testing if p and q are prime is not necessary since the dsa.generate_parameters function guarantees
+# that p and q are prime. 
+
+# Test if g is a generator of the subgroup of order q in Z_p* (i.e., g^q mod p == 1)
+def test_generator_properties():
+    p, q, g = src.crypto_utils.generate_schnorr_group()
+    assert pow(g, q, p) == 1
+
+# Test if p = k*q + 1 for some integer k (i.e., p-1 is divisible by q)
+def test_group_order():
+    p, q, g = src.crypto_utils.generate_schnorr_group()
+    assert (p - 1) % q == 0
+    
