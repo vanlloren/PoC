8522efd85df7886a9c2f15e2e6594df73891c183
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-02-25 20:23:15 +0100

    Created Message utility

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+# This file contains utilities
+
+from dataclasses import dataclass
+
+class Message:
+    description: str   # commitment, key, etc.
+    sender: int  # 1 for user1, 2 for user2, 3 for recovery party
+    receiver: int  # 1 for user1, 2 for user2, 3 for recovery party
+    content: any
+    
