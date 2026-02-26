7d79bce617479179f8e4964d75d40261e22f081f
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-02-26 22:11:25 +0100

    Fix on Message dataclass and added ProtocolAbortedException for better error handling.

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,13 @@
 
 from dataclasses import dataclass
 
+@dataclass
 class Message:
     description: str   # commitment, key, etc.
     sender: int  # 1 for user1, 2 for user2, 3 for recovery party
     receiver: int  # 1 for user1, 2 for user2, 3 for recovery party
     content: any
+
+class ProtocolAbortedException(Exception):
+    pass
     
