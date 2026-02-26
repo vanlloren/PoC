90d95437012bd10e68d61c8fad991db0bbe9eb31
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-02-26 22:13:15 +0100

    Thread managemente features introduced in key generation handling. New message queue for exception handling. Small fixes in key generation handling.

--- src/general_procedures.py
+++ src/general_procedures.py
@@ -5,25 +5,27 @@ import src.entities
 import threading
 import time
 import queue
+import src.utils
 
 def initialize_protocol():
     # Create queues for communication between the parties
     queue1 = queue.Queue()  # User1
     queue2 = queue.Queue()  # User2
     queue3 = queue.Queue()  # Recovery Party
+    exceptionQueue = queue.Queue()  # To communicate exceptions from threads to the main thread
 
     # Initialize the recovery party and the users
-    recovery_party = RecoveryParty()
-    user1 = User1(party_id=1)
-    user2 = User2(party_id=2)
+    recovery_party = src.entities.RecoveryParty()
+    user1 = src.entities.User1(party_id=1)
+    user2 = src.entities.User2(party_id=2)
 
     # Send the queues to the users and the recovery party
-    user1.set_communication_queues(queue1, queue2, queue3)
-    user2.set_communication_queues(queue1, queue2, queue3)
-    recovery_party.set_communication_queues(queue1, queue2, queue3)
+    user1.set_communication_queues(queue1, queue2, queue3, exceptionQueue)
+    user2.set_communication_queues(queue1, queue2, queue3, exceptionQueue)
+    recovery_party.set_communication_queues(queue1, queue2, queue3, exceptionQueue)
 
     # Generate group parameters (p, q, g) for the protocol
-    p, q, g = src.crypto_utils.generate_group_parameters()
+    p, q, g = src.crypto_utils.generate_schnorr_group()
 
     # Set the group parameters for all the parties
     recovery_party.set_group_parameters(p, q, g)
@@ -37,16 +39,23 @@ def initialize_protocol():
     user1.start()
     user2.start()
 
-    return recovery_party, user1, user2
+    return recovery_party, user1, user2, exceptionQueue
 
 # This function aborts the protocol 
 def abort():
-    print("Protocol aborted due to an error or malicious behavior.")
-    exit()
+    raise src.utils.ProtocolAbortedException("Protocol aborted due to an error or malicious behavior.")
 
 # This function simulates the key generation protocol between two users
 def begin_keygen_protocol(user1, user2):
     # Send to user1 and user2 the message to start the key generation protocol
-    msg = Message(description="start_keygen", sender=0, receiver=0, content=None)
+    msg = src.utils.Message(description="start_keygen", sender=0, receiver=0, content=None)
     user1.queue1.put(msg)
     user2.queue2.put(msg)
+
+    timeout = 10  # secondi
+    success1 = user1.keygen_completed.wait(timeout)
+    success2 = user2.keygen_completed.wait(timeout)
+    
+    
+    
+    return success1 and success2
