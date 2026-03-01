7fd1d0f50b7dd2df7a0d0ef5dbac947373c7ca3c
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-03-01 18:52:18 +0100

    Main modified to allow simulation of ordinary signature phase.

--- src/main.py
+++ src/main.py
@@ -11,12 +11,12 @@ import queue
 def main():
     # Initialize the protocol, perform key generation and get the recovery party and the users
     try:
-        recovery, user1, user2, exceptionQueue = src.general_procedures.initialize_protocol()
+        recovery, user1, user2, abortExceptionQueue, failedSignatureExceptionQueue = src.general_procedures.initialize_protocol()
         success = src.general_procedures.begin_keygen_protocol(user1, user2)
 
         try:
-            # Check if any of the threads has put an exception in the exceptionQueue
-            exc = exceptionQueue.get_nowait()
+            # Check if any of the threads has put an exception in the abortExceptionQueue
+            exc = abortExceptionQueue.get_nowait()
             raise exc
         except queue.Empty:
             pass
@@ -28,18 +28,24 @@ def main():
         else:
             print("Key generation failed!")
 
+
+        # Now simulation of an ordinary signature
+        src.general_procedures.sign(user1, user2, "This is a message to be signed.", failedSignatureExceptionQueue, abortExceptionQueue)
+        
+        # Another signature, to show that the protocol can be used multiple times with the same keys
+        src.general_procedures.sign(user1, user2, "This is another message to be signed.", failedSignatureExceptionQueue, abortExceptionQueue)
+
     except ProtocolAbortedException as e:
         raise e
         print(f"Main: protocollo abortito - {e}")
     except Exception as e:
         print(f"Main: errore inaspettato - {e}")
-    
-
-
-    # Alla fine di tutto, fermiamo i thread dei party
-    recovery.running = False
-    user1.running = False
-    user2.running = False
+    finally:
+        # Alla fine di tutto, fermiamo i thread dei party
+        #recovery.running = False
+        #user1.running = False
+        #user2.running = False
+        True
 
 if __name__ == "__main__":
     main()
