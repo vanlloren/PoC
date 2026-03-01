5aa8cfe81e6141e92dfe3d86ec72a8ed2e004d0a
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-03-01 19:49:06 +0100

    Fixed tests for malicious signature failures (no abortion)

--- tests/test_malicious_signature.py
+++ tests/test_malicious_signature.py
@@ -5,6 +5,7 @@ import pytest
 import unittest.mock as mock
 from src.entities import User1, User2, RecoveryParty
 import src.general_procedures
+import src.main
 
 # This test mimicks a signature failure by player 2 before or inside signature_1
 def test_malicious_signature_before_signature_1_user2():
@@ -39,13 +40,13 @@ def test_malicious_signature_before_signature_1_user2():
         if (msg.description == "s_1_decommitment" and msg.sender == 1) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)
 
-        with mock.patch_object(User2, "process_message", new=mock_process_message_user2):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User2, "processMessage", new=mock_process_message_user2):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 2 before or inside signature_2
 def test_malicious_signature_before_signature_2_user2():
@@ -80,13 +81,13 @@ def test_malicious_signature_before_signature_2_user2():
         if (msg.description == "s_1_decommitment" and msg.sender == 1) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)
 
-        with mock.patch_object(User2, "process_message", new=mock_process_message_user2):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User2, "processMessage", new=mock_process_message_user2):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 2 before or inside signature_3
 def test_malicious_signature_before_signature_3_user2():
@@ -121,13 +122,13 @@ def test_malicious_signature_before_signature_3_user2():
         if (msg.description == "s_1_decommitment" and msg.sender == 1) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)
 
-        with mock.patch_object(User2, "process_message", new=mock_process_message_user2):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User2, "processMessage", new=mock_process_message_user2):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 2 before or inside signature_4
 def test_malicious_signature_before_signature_4_user2():
@@ -162,13 +163,13 @@ def test_malicious_signature_before_signature_4_user2():
         if (msg.description == "s_1_decommitment" and msg.sender == 1) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)
 
-        with mock.patch_object(User2, "process_message", new=mock_process_message_user2):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User2, "processMessage", new=mock_process_message_user2):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 2 before or inside combine
 def test_malicious_signature_before_combine_user2():
@@ -203,13 +204,13 @@ def test_malicious_signature_before_combine_user2():
             # Do not call combine, but put an exception in the failedSignatureExceptionQueue
             src.general_procedures.raise_signature_exception(self.party_id)
 
-        with mock.patch_object(User2, "process_message", new=mock_process_message_user2):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User2, "processMessage", new=mock_process_message_user2):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 1 before or inside signature_1
 def test_malicious_signature_before_signature_1_user1():
@@ -244,13 +245,13 @@ def test_malicious_signature_before_signature_1_user1():
         if (msg.description == "s_2_decommitment" and msg.sender == 2) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)  
 
-        with mock.patch_object(User1, "process_message", new=mock_process_message_user1):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User1, "processMessage", new=mock_process_message_user1):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 1 before or inside signature_2
 def test_malicious_signature_before_signature_2_user1():
@@ -285,13 +286,13 @@ def test_malicious_signature_before_signature_2_user1():
         if (msg.description == "s_2_decommitment" and msg.sender == 2) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)  
 
-        with mock.patch_object(User1, "process_message", new=mock_process_message_user1):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User1, "processMessage", new=mock_process_message_user1):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 1 before or inside signature_3
 def test_malicious_signature_before_signature_3_user1():
@@ -326,13 +327,13 @@ def test_malicious_signature_before_signature_3_user1():
         if (msg.description == "s_2_decommitment" and msg.sender == 2) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)  
 
-        with mock.patch_object(User1, "process_message", new=mock_process_message_user1):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User1, "processMessage", new=mock_process_message_user1):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 1 before or inside signature_4
 def test_malicious_signature_before_signature_4_user1():
@@ -367,13 +368,13 @@ def test_malicious_signature_before_signature_4_user1():
         if (msg.description == "s_2_decommitment" and msg.sender == 2) or (msg.description == "s_3_decommitment" and msg.sender == 3):
             self.combine(msg.content)  
 
-        with mock.patch_object(User1, "process_message", new=mock_process_message_user1):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
-                pytest.fail("Expected SignatureException was not raised")
+    with mock.patch.object(User1, "processMessage", new=mock_process_message_user1):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
+            pytest.fail("Expected SignatureException was not raised")
 
 # This test mimicks a signature failure by player 1 before or inside combine
 def test_malicious_signature_before_combine_user1():
@@ -408,10 +409,11 @@ def test_malicious_signature_before_combine_user1():
             # Do not call combine, but put an exception in the failedSignatureExceptionQueue
             src.general_procedures.raise_signature_exception(self.party_id)
 
-        with mock.patch_object(User1, "process_message", new=mock_process_message_user1):
-            try:
-                src.main.main()
-            except src.utils.SignatureException as e:
-                assert True
-            else:
+    with mock.patch.object(User1, "processMessage", new=mock_process_message_user1):
+        try:
+            src.main.main()
+        except src.utils.SignatureException as e:
+            assert True
+        else:
                 pytest.fail("Expected SignatureException was not raised")
+        
